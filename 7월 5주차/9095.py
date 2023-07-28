06f5132abd9dadf771b3b48d6ac38ae27588f87d
wonguk <jcc9660@naver.com>
2023-07-29 07:42:12 +0900

    Add: 7/29

---
+++ 7월 5주차/9095.py
@@ -0,0 +1,15 @@
+import sys
+path = __file__[0:-2] + 'txt'
+sys.stdin = open(path, 'r')
+input = sys.stdin.readline
+
+n = int(input())
+nums = []
+answer = []
+def sol(num):
+    return
+
+for i in range(n):
+    sol(int(input()))
+
+
