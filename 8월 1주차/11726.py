f702b64a4f43e39b343da1f595ed9215a5c538d3
wonguk <jcc9660@naver.com>
2023-08-07 23:05:10 +0900

    Add: 8/7

---
+++ 8월 1주차/11726.py
@@ -0,0 +1,15 @@
+import sys
+path = __file__[0:-2] + 'txt'
+sys.stdin = open(path, 'r')
+input = sys.stdin.readline
+a = map(int,input().split())
+
+def solution(n):
+    dp = [0 for i in range(n)]
+    dp[0], dp[1] = 1, 2
+    for i in range(2, n):
+        dp[i] = (dp[i-1] + dp[i-2]) % 1000000007
+
+    return dp[n-1]
+
+solution(a)
